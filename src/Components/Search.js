668c0fc831b5b09173b5b722b77bebf06c12cde6
Ameerusa86 <engamermecha@gmail.com>
2023-03-03 14:26:41 -0500

    Addinf some bootstrap style to the search bar

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,27 +1,35 @@
 import React from "react";
 import "./Search.css";
 import "../index.css";
+import { Col, Row, Container } from "react-bootstrap";
 
 const Search = () => {
   return (
     <section className="search">
-      <form className="control">
-        <input
-          type="search"
-          name="search"
-          id="search"
-          placeholder="Search a country"
-        />
-      </form>
-      <div className="selector">
-        <select name="select" id="select" className="select">
-          <option value="Asia">Asia</option>
-          <option value="Africa">Africa</option>
-          <option value="Europe">Europe</option>
-          <option value="North America">North America</option>
-          <option value="South America">South America</option>
-        </select>
-      </div>
+      <Row>
+        <Col xs={12} md={8}>
+          <form className="control">
+            <input
+              type="search"
+              name="search"
+              id="search"
+              placeholder="Search a country"
+            />
+          </form>
+        </Col>
+        <Col>
+          <div className="selector">
+            <select name="select" id="select" className="select">
+              <option value="Asia">Select Region</option>
+              <option value="Asia">Asia</option>
+              <option value="Africa">Africa</option>
+              <option value="Europe">Europe</option>
+              <option value="North America">North America</option>
+              <option value="South America">South America</option>
+            </select>
+          </div>
+        </Col>
+      </Row>
     </section>
   );
 };
